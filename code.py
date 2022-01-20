af4efdf8fdc2e3346f0a124220a9abc17656838f
p-ram <94652181+p-ram@users.noreply.github.com>
2022-01-19 16:03:24 -0800

    Update code.py

--- code.py
+++ code.py
@@ -5,11 +5,7 @@ input text_in.txt, if the word ‘THE’ (case-insensitive) occurs even number o
 with all the words staring with – ‘a,e,i,o,u’ in UPPER CASE. All other words should be in lower case.
 If, however, the number of occurrences of the word ‘THE’ (case-insensitive) is odd then sentence should be written as it is in the text_in.txt file.
 """
-# -*- coding: utf-8 -*-
-"""
-input text_in.txt, if the word ‘THE’ (case-insensitive) occurs even number of times you should rewrite the words in the paragraph with all the words staring with – ‘a,e,i,o,u’ in UPPER CASE. All other words should be in lower case. If the word ‘THE’ does not occur in a sentence, then you should still rewrite the words in the paragraph with all the words staring with – ‘a,e,i,o,u’ in UPPER CASE. All other words should be in lower case.
-If, however, the number of occurrences of the word ‘THE’ (case-insensitive) is odd then sentence should be written as it is in the text_in.txt file.
-"""
+
 import re
 
 textin=open("text_in.txt","r")
